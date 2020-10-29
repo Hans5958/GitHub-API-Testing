846ce7f66f413995568146f487f2fd9e3d903bda
Hans5958 <email5958.1@gmail.com>
2020-10-29 18:34:13 +0700

    Creating changelog workflow (ScratchAddons/website)
    master branch
    Update changelog
    final 2
    final 1
    workflow done hopefully
    1
    2
    initial files
    Creating changelog workflow (ScratchAddons/website)

---
+++ changelog.sh
@@ -0,0 +1,46 @@
+# SOURCE=$(curl https://raw.githubusercontent.com/ScratchAddons/contributors/master/README.md)
+
+SAVEIFS=$IFS
+IFS=$'\n'
+
+DO_PRINT_1=true
+DESTINATION_FILE="site/changelog.md"
+
+print_line() {
+	echo $1 >> "$DESTINATION_FILE.tmp"
+}
+
+while read -r line1; do
+   	if [[ $line1 == "<!-- sa-changelog-end -->" ]]; then
+		DO_PRINT_1=true
+	fi
+	if [[ $DO_PRINT_1 = true ]]; then 
+		print_line "$line1"
+	fi
+	if [[ $line1 == "<!-- sa-changelog-start -->" ]]; then
+		DO_PRINT_1=false
+		DO_PRINT_2=false
+		while read -r line2; do
+			if [[ $line2 == "<!-- sa-changelog-end -->" ]]; then
+				DO_PRINT_2=false
+			fi
+			if [[ $DO_PRINT_2 = true ]]; then 
+				print_line "$line2"
+			fi
+			if [[ $line2 == "<!-- sa-changelog-start -->" ]]; then
+				DO_PRINT_2=true
+			fi
+		done <<< "$(curl https://raw.githubusercontent.com/ScratchAddons/ScratchAddons/GrahamSH-LLK-changelog/CHANGELOG.md)"
+	fi
+
+done <<< "$(tr -d '\r' < $DESTINATION_FILE)"
+
+IFS=$SAVEIFS
+
+perl -pi -e 's/\n/\r\n/g' "$DESTINATION_FILE.tmp"
+rm -rf "$DESTINATION_FILE"
+mv "$DESTINATION_FILE.tmp" "$DESTINATION_FILE"
+
+# SOURCE=${RAW_SOURCE%$'\n'*}
+# echo $SOURCE
+# echo $SOURCE | grep "<!-- ALL-CONTRIBUTORS-LIST:END -->"
